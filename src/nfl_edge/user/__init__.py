12422a67557434463a7e8bb0e2a152573bf6ab2e
Tkcool28 <toddkirschman@gmail.com>
2026-08-22 00:35:05 -0600

    feat: add nfl edge user staking package

---
+++ src/nfl_edge/user/__init__.py
@@ -0,0 +1,5 @@
+"""User-level account state for NFL EDGE."""
+
+from .staking_profile import FlatStakeMode, StakingStrategy, UserStakingProfile
+
+__all__ = ["FlatStakeMode", "StakingStrategy", "UserStakingProfile"]
