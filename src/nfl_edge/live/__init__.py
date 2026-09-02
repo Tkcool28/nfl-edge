a1887a26b29efc645b4d22e3a64ef51c4b28e0e2
Tkcool28 <toddkirschman@gmail.com>
2026-09-02 12:41:01 -0600

    Add live football package

---
+++ src/nfl_edge/live/__init__.py
@@ -0,0 +1,4 @@
+"""Prospective NFL EDGE live football scoring infrastructure."""
+from .week1_2026 import load_week1_schedule, schedule_to_frame, validate_week1_schedule
+
+__all__ = ["load_week1_schedule", "schedule_to_frame", "validate_week1_schedule"]
