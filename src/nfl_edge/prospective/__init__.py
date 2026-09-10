5580bf98d2bc902124149620ec57601f6817919a
Tkcool28 <toddkirschman@gmail.com>
2026-09-10 14:29:08 -0600

    feat: add prospective card tracking foundation

---
+++ src/nfl_edge/prospective/__init__.py
@@ -0,0 +1,25 @@
+"""Prospective production-evidence tracking for NFL EDGE."""
+
+from .card_tracking_v1 import (
+    AppendOnlyViolation,
+    ProspectiveCardError,
+    build_pending_results,
+    build_publication_snapshot,
+    build_summary,
+    derive_episodes,
+    realized_profit_units,
+    resolve_official,
+    write_publication_snapshot,
+)
+
+__all__ = [
+    "AppendOnlyViolation",
+    "ProspectiveCardError",
+    "build_pending_results",
+    "build_publication_snapshot",
+    "build_summary",
+    "derive_episodes",
+    "realized_profit_units",
+    "resolve_official",
+    "write_publication_snapshot",
+]
